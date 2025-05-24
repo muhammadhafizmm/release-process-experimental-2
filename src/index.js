c742103e65aec540f8278314a051cea1bb459124
muhammadhafizmm <m.maulana@traveloka.com>
2025-05-24 16:12:43 +0700

    feat: 3001

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const url = require("url");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const server = http.createServer((req, res) => {
   const { pathname } = url.parse(req.url, true);
